c9db45b83c2ea8eb9982d320cd01d29873d5987d
lakhan sharan <lakhansharan946@gmail.com>
2025-07-24 01:19:31 +0530

    changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: ['https://chatify-prat.onrender.com'],
     methods: ['GET', 'POST']
   }
 });
